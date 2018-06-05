b474f29d8aae061fb3cf649afd25dff5e4d432b1
Abhijit Kumar <abhijitstack@gmail.com>
2018-06-06 00:14:34 +0530

    PrimitiveDataType define

---
+++ HelloWorld/src/DataType/PrimitiveDataTypes.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package DataType;
+
+/**
+ * @author Abhijit Kumar
+ *
+ */
+public class PrimitiveDataTypes {
+
+	public void showInt() {
+
+	}
+
+	public void showChar() {
+
+	}
+
+	public void showFloat() {
+
+	}
+	
+	public void showDouble() {
+
+	}
+	
+	public void showBoolean() {
+
+	}
+	
+	public void showByte() {
+
+	}
+	
+	public void showShort() {
+
+	}
+	
+	public void showLong() {
+
+	}
+
+}
