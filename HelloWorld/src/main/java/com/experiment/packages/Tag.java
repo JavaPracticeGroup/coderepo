54794d3555c0001d66e234bd65f02c7f4c7c229a
Abhijit Kumar <abhijit.stack@gmail.com>
2018-06-18 19:12:52 +0530

    Tag class

---
+++ HelloWorld/src/main/java/com/experiment/packages/Tag.java
@@ -0,0 +1,10 @@
+package com.experiment.packages;
+
+public final class Tag {
+	
+	public final static String NOTE ="__NOTE__: ";
+	public final static String EXCEPT ="__EXCEPT__: ";
+	public final static String WARN ="__WARN__: ";
+	public final static String IMPORTANT ="__IMPORTANT__: ";
+	public final static String RULE ="__RULE__: ";
+}
